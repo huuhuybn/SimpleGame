e1c6005a33ff7ac03e19fcd625169d22f2fc906a
HuyNguyen <huuhuybn@gmail.com>
2019-11-06 10:40:22 +0700

    Initial commit

---
+++ app/src/main/java/com/dotplays/kilobolt/framework/Image.java
@@ -0,0 +1,10 @@
+package com.dotplays.kilobolt.framework;
+
+import com.dotplays.kilobolt.framework.Graphics.ImageFormat;
+
+public interface Image {
+    public int getWidth();
+    public int getHeight();
+    public ImageFormat getFormat();
+    public void dispose();
+}
---
+++ app/src/main/java/com/dotplays/kilobolt/robotgame/SplashLoadingScreen.java
@@ -0,0 +1,47 @@
+package com.dotplays.kilobolt.robotgame;
+
+import com.dotplays.kilobolt.framework.Game;
+import com.dotplays.kilobolt.framework.Graphics;
+import com.dotplays.kilobolt.framework.Screen;
+import com.dotplays.kilobolt.framework.Graphics.ImageFormat;
+
+public class SplashLoadingScreen extends Screen {
+	public SplashLoadingScreen(Game game) {
+		super(game);
+	}
+
+	@Override
+	public void update(float deltaTime) {
+		Graphics g = game.getGraphics();
+		Assets.splash= g.newImage("splash.jpg", ImageFormat.RGB565);
+
+		
+		game.setScreen(new LoadingScreen(game));
+
+	}
+
+	@Override
+	public void paint(float deltaTime) {
+
+	}
+
+	@Override
+	public void pause() {
+
+	}
+
+	@Override
+	public void resume() {
+
+	}
+
+	@Override
+	public void dispose() {
+
+	}
+
+	@Override
+	public void backButton() {
+
+	}
+}
